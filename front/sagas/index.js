d459c7dbb2d8df8bb2800969960cc785ce980186
hyoilll <50327128+hyoilll@users.noreply.github.com>
2021-09-10 13:30:28 +0900

    take, while take, takeEvery, takeLatest, throttle

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -1,4 +1,13 @@
-import { all, fork, take, call, put } from "redux-saga/effects";
+import {
+  all,
+  fork,
+  take,
+  call,
+  put,
+  takeEvery,
+  takeLatest,
+  delay,
+} from "redux-saga/effects";
 import axios from "axios";
 
 function logInAPI(data) {
@@ -11,7 +20,8 @@ function logInAPI(data) {
 // loginAPI(action.data) ==> call(logInAPI, action.data)
 function* logIn(action) {
   try {
-    const result = yield call(logInAPI, action.data);
+    // const result = yield call(logInAPI, action.data);
+    yield delay(1000);
     yield put({
       type: "LOG_IN_SUCCESS",
       data: result.data, // 성공결과가 담겨있음
@@ -30,7 +40,8 @@ function logOutAPI() {
 
 function* logOut() {
   try {
-    const result = yield call(logOutAPI);
+    // const result = yield call(logOutAPI);
+    yield delay(1000);
     yield put({
       type: "LOG_OUT_SUCCESS",
       data: result.data, // 성공결과가 담겨있음
@@ -49,7 +60,8 @@ function addPostAPI(data) {
 
 function* addPost(action) {
   try {
-    const result = yield call(addPostAPI, action.data);
+    // const result = yield call(addPostAPI, action.data);
+    yield delay(1000);
     yield put({
       type: "ADD_POST_SUCCESS",
       data: result.data, // 성공결과가 담겨있음
@@ -63,15 +75,15 @@ function* addPost(action) {
 }
 
 function* watchLogin() {
-  yield take("LOG_IN_REQUEST", logIn); // LOG_IN 액션이 실행되면 logIn함수가 실행
+  yield takeLatest("LOG_IN_REQUEST", logIn); // LOG_IN 액션이 실행되면 logIn함수가 실행}
 }
 
 function* watchLogOut() {
-  yield take("LOG_OUT_REQUEST", logOut);
+  yield takeLatest("LOG_OUT_REQUEST", logOut);
 }
 
 function* watchAddPost() {
-  yield take("ADD_POST_REQUEST", addPost);
+  yield takeLatest("ADD_POST_REQUEST", addPost);
 }
 
 export default function* rootSaga() {
@@ -81,5 +93,15 @@ export default function* rootSaga() {
 // all : 매개변수로서 배열을 받고, 배열안의 원소들을 한번에 실행시켜줌
 // fork : 비동기 함수를 실행시켜줌 / 서버에서의 결과값을 기다리지 않고 그대로 실행함
 // call : 동기 함수를 실행시켜줌 / 서버에서의 결과값을 기다림 await과 같은 효과
-// take : 매개변수의 액션이 실행될 때 까지 기다리겠다는 것
 // put : dispatch와 같은 역할, 액션을 디스패치해줌
+// take : (1회용) 매개변수의 액션이 실행될 때 까지 기다리겠다는 것
+//  1. while take : 동기적으로 동작
+//  2. takeEvery : 비동기적으로 동작
+// takeLatest : 실수로 여러번 발생된 액션을 마지막 액션 1번으로 취급해주는 것
+//              3개의 액션을 서버로 요청하면 서버로부터 응답을 1개로 줄여주는 것이지, 요청까지 1개로 줄여주진 않음 => 서버에 3개의 액션 기록이 남음
+//              => 서버쪽 검사가 필요함 (이미 등록한 액션이기 때문에 등록되지 않습니다. 와 같은 메세지를 안내함)
+//              위의 문제를 해결해준 것이 "throttle" 지정한 시간당 한번밖에 요청을 할 수 없도록 제한을 둠
+//  ex) 3번의 액션발생하였을 때 앞의 2번의 액션이 이미 완료된 것이면 적용되고
+//      2번의 액션이 완료되지 않고 마지막 액션과 같이 액션이 발생된 것이라면 앞의 2개의 액션은 적용안됨
+// throttling / debouncing의 차이
+// https://www.zerocho.com/category/JavaScript/post/59a8e9cb15ac0000182794fa
